70741e376c0abc97f7415c8c8dea84cdccfd0f5b
Mahmoud <mahmoud.mohamed30120@gmail.com>
2022-04-19 01:19:34 +0300

    add base html

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name = "store"
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('home/', views.index, name="home"),
     path('gallery/', views.gallery, name="gallery"),
     path('<int:page_num>', views.num_router, name="num_router"),
-    path('<str:topic>/', views.router, name="page"),
     # path('<str:topic>/<content>/', views.new_route),
 ]
--- home/views.py
+++ home/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
-from static_precompiler.utils import compile_static
-compile_static("styles.scss")
 
 # Create your views here.
 
 routes = {
-    "home": "home",
+
     "gallery": "gallery",
     "info": "info",
 }
 
 
 def index(request):
-    return render(request, "home/index.html")
+    path = request.path
+    nav_items = {"keys": routes,
+                 'path': path,
+                 }
+    return render(request, "home/index.html", context=nav_items)
 
 
 def gallery(request):
-    return render(request, "home/gallery.html")
+    path = request.path
+    nav_items = {"keys": routes,
+                 'path': path,
+                 }
+    return render(request, "home/gallery.html", context=nav_items)
 
 
 def num_router(request, page_num):
@@ -30,11 +36,11 @@ def num_router(request, page_num):
         raise Http404("Page not found")
 
 
-def router(request, topic):
-    try:
-        return HttpResponse(str(routes[topic]))
-    except Exception as e:
-        raise Http404(f"Page {e} not found")
+# def router(request, topic):
+#     try:
+#         return HttpResponse(str(routes[topic]))
+#     except Exception as e:
+#         raise Http404(f"Page {e} not found")
 
 # def new_route(request, topic, content):
 #     routes[topic] = content
