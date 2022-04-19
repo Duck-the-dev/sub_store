5de4f9317fec7a8fa861f5d2f4759bd0458d9014
Mahmoud <mahmoud.mohamed30120@gmail.com>
2022-04-20 00:45:15 +0300

    current  version

--- sub_store/urls.py
+++ sub_store/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include("home.urls")),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ]
 
 
 # handler404 = 'home.views.error_404'
